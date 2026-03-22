1c44393961bf72a17ddbb3cabb6e0dade2f97eb0
bidolsermikayel1-ux <bidolsermikayel1@gmail.com>
2026-03-22 12:09:04 +0800

    Add files via upload

---
+++ Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Student4API.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Office { get; set; }
+    }
+}
