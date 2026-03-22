7dd35a00dd744281b2a5bc2b90872005d68c74d5
bidolsermikayel1-ux <bidolsermikayel1@gmail.com>
2026-03-22 12:06:32 +0800

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student4API.Models;
+
+
+namespace Student4API.Data
+{
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Enrollment> Enrollments { get; set; }
+    public DbSet<Instructor> Instructors { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Section> Sections { get; set; }
+}
+}
